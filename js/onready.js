205a86e8ea82f322a883e2aa91d5c61557333469
poppy <horpoppy@gmail.com>
2019-04-07 21:03:40 +0800

    Competition 模板

--- js/onready.js
+++ js/onready.js
@@ -1,3 +1,4 @@
+var hasChangeDescirption = 0;
 $(document).ready(function () {
     $("#left_side_bar").css("min-height",$(window).height());
     $("#my-video").width($("#my-video").parent().width());
@@ -12,10 +13,38 @@ $(document).ready(function () {
     });
     $("#wechat").click();
     $("#wechat").click();
+    if($(window).width() < 992){
+        $(".left-description").each(function () {
+            $(this).parent().next().after($(this).parent());
+        });
+        hasChangeDescirption = 1;
+    }else{
+        $(".left-description").each(function () {
+            $(this).parent().prev().before($(this).parent());
+        });
+        hasChangeDescirption = 0;
+    }
 });
 $(window).resize(function () {
     $("#my-video").width($("#my-video").parent().width());
     $("#left_side_bar").css("min-height",$(window).height());
+    if($(window).width() < 992){
+        if(hasChangeDescirption == 0){
+            console.log("des == 0");
+            $(".left-description").each(function () {
+                $(this).parent().next().after($(this).parent());
+            });
+            hasChangeDescirption = 1;
+        }
+    }else{
+        if(hasChangeDescirption == 1){
+            console.log("des == 1");
+            $(".left-description").each(function () {
+                $(this).parent().prev().before($(this).parent());
+            });
+            hasChangeDescirption = 0;
+        }
+    }
 });
 $(window).scroll(function () {
     // $("#left_sidebar").css("height",$(window).height());
